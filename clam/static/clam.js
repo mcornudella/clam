e9b07c804eaecf275eb240f366da593d3657b13a
mcornudella <miquel.cornudella@gmail.com>
2018-12-18 15:15:05 +0100

    change upload behaviour

--- clam/static/clam.js
+++ clam/static/clam.js
@@ -805,10 +805,11 @@ function initclam() { //eslint-disable-line no-unused-vars, complexity
                      uploader._settings.data.inputtemplate = inputtemplate_id;
                      addformdata( '#uploadparameters', uploader._settings.data );
                  }
+                 $('#clientupload').show();
             },
             onSubmit: function(){
                 $('#clientupload').show();
-                $('#uploadprogress').show();
+                $('#uploadprogress').hide();
             },
             onComplete: function(file, response){ //eslint-disable-line no-unused-vars
                 processuploadresponse(response, "#uploadparameters");
