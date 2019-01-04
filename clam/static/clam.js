a50de9e5f8e09bb1f09e5efddc35d6c09102ec3d
mcornudella <miquel.cornudella@gmail.com>
2019-01-04 12:23:25 +0100

    modified input hover

--- clam/static/clam.js
+++ clam/static/clam.js
@@ -146,7 +146,7 @@ function deleteinputfile(filename) {   //eslint-disable-line no-unused-vars
     	{
     		document.getElementById("buttonstartbutton").disabled = true;
     		document.getElementById("buttonstartbutton").style.background="#686868";
-    		document.getElementById("buttonstartbutton").injectStyles('a:hover { color: grey; }');
+    		injectStyles('input:hover { color: grey; }');
     	}
 }
 
@@ -283,7 +283,7 @@ function processuploadresponse(response, paramdiv) {
                 	{
                 	document.getElementById("buttonstartbutton").disabled = false;
                 	document.getElementById("buttonstartbutton").style.background="#8a4949";
-                	document.getElementById("buttonstartbutton").injectStyles('a:hover { color: #aa5e5f; }');
+                	injectStyles('input:hover { color: #aa5e5f; }');
                 	}
             }
 
