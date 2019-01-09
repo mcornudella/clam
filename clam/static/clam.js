14986120f2cc217cd582d79c40ea4d63dcead365
mcornudella <miquel.cornudella@gmail.com>
2019-01-09 11:19:51 +0100

    test

--- clam/static/clam.js
+++ clam/static/clam.js
@@ -278,8 +278,8 @@ function processuploadresponse(response, paramdiv) {
 
             //Add this file to the input table if it doesn't exist yet
             if (!found) {
-            	console.log($(this).find('parameters'));
-            	tableinputfiles.fnAddData( [  '<a href="' + baseurl + '/' + project + '/input/' + $(this).attr('filename') + '">' + $(this).attr('filename') + '</a>', $(this).attr('templatelabel'), $(this).find('parameters'),'<img src="' + baseurl + '/static/delete.png" title="Delete this file" onclick="deleteinputfile(\'' + $(this).attr('filename') + '\');" />' ] );
+            	
+            	tableinputfiles.fnAddData( [  '<a href="' + baseurl + '/' + project + '/input/' + $(this).attr('filename') + '">' + $(this).attr('filename') + '</a>', $(this).attr('templatelabel'), $(this).find('parameters').values(),'<img src="' + baseurl + '/static/delete.png" title="Delete this file" onclick="deleteinputfile(\'' + $(this).attr('filename') + '\');" />' ] );
                 //tableinputfiles.fnAddData( [  '<a href="' + baseurl + '/' + project + '/input/' + $(this).attr('filename') + '">' + $(this).attr('filename') + '</a>', $(this).attr('templatelabel'), $(this).attr('format') ,'<img src="' + baseurl + '/static/delete.png" title="Delete this file" onclick="deleteinputfile(\'' + $(this).attr('filename') + '\');" />' ] );
                 if(tableinputfiles.fnGetData().length != 0) 
                 	{
